7b45aabf495e66a4ef4d86cb6cadee4a235aa16b
fgrey17 <fcgr@hotmail.com>
2015-05-11 11:58:21 -0700

    Added Gradient to Constraint Bar

--- src/ConstraintBar.java
+++ src/ConstraintBar.java
@@ -35,7 +35,9 @@ public class ConstraintBar extends GCompound {
 	
 	public ConstraintBar(double totalSoft, double totalHard) {
 		bar = new GRect(BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
-		bar.setFillColor(Color.BLUE);
+		//bar.setFillColor(Color.BLUE);
+		//bar.setFilled(true);
+		paint(bar);
 		soft = new GLabel("SOFT", BAR_X-50, BAR_HEIGHT+BAR_Y);
 		hard = new GLabel("HARD", BAR_WIDTH+BAR_X+20, BAR_HEIGHT+BAR_Y);
 		totalSoftConstraints = totalSoft;
@@ -45,7 +47,25 @@ public class ConstraintBar extends GCompound {
 		add(soft);
 		add(hard);
 	}
-	
+	public void paint(GRect cBar) {
+	  Color color1 = Color.GREEN;
+	  Color color2 = Color.RED;
+	  int steps = BAR_HEIGHT/10;
+	  int stepHeight = 10;
+	  
+	  for (int i=0; i<steps; i++){
+		  float ratio = (float) i / (float) steps;
+          int red = (int) (color2.getRed() * ratio + color1.getRed() * (1 - ratio));
+          int green = (int) (color2.getGreen() * ratio + color1.getGreen() * (1 - ratio));
+          int blue = (int) (color2.getBlue() * ratio + color1.getBlue() * (1 - ratio));
+          Color stepColor = new Color(red, green, blue);
+          GRect stepBar = new GRect (BAR_X, BAR_Y+(i*stepHeight), BAR_WIDTH,stepHeight);
+          stepBar.setFillColor(stepColor);
+          stepBar.setFilled(true);
+          add(stepBar);
+	  }
+	}
+
 	public void setSoftSatisfied(double satisfied) {
 		softSatisfied = satisfied;
 		soft.setLocation(BAR_X-50, getSoftSatisfaction());
